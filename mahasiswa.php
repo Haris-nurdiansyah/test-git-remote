73e5cd512965b13cc7e2c79bf6bb24506ffd3ad7
haris-nurdiansyah <haris00santos@gmail.com>
2020-06-22 10:16:57 +0700

    merubah file mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,12 +1,15 @@
-<html>
-  <head>
-    <title> Mahasiswa List </title>
-  </head>
-  <body>
-    <h1>List Mahasiswa</h1>
-    <ul>
-      <li>Haris Nurdaiansayah</li>
-      <li>Dini Aulia</li>
-    </ul>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>List Mahasiswa</title>
+</head>
+<body>
+  <h1>List Mahasiswa</h1>
+  <ul>
+    <li>Haris Nurdiansyah</li>
+    <li>Dini Aulia</li>
+  </ul>
+</body>
 </html>
