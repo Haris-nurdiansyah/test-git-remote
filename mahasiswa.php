05e5257d7b7757c20ff8dac2f8f1950328ff6b5f
Haris-nurdiansyah <66940598+Haris-nurdiansyah@users.noreply.github.com>
2020-06-22 10:12:25 +0700

    menambahkan file mahasiswa.php

---
+++ mahasiswa.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title> Mahasiswa List </title>
+  </head>
+  <body>
+    <h1>List Mahasiswa</h1>
+    <ul>
+      <li>Haris Nurdaiansayah</li>
+      <li>Dini Aulia</li>
+    </ul>
+  </body>
+</html>
